5a05a4dd6b91d1d46d590ca7a3b1906acdd3b389
Brunno Lorran <brunnolorran.silva@gmail.com>
2020-11-17 21:49:20 -0300

    created component Login with antd

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,71 @@
+import { Form, Input, Button } from "antd";
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
 const Login = () => {
-  return;
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Usuário"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Porfavor preencha o seu usuário!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Senha"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Porfavor preencha a sua senha!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Logar
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default Login;
